c65b91989dc0116a4daa960a4d8ebc2aeea364bc
Harvastum <harvastum@gmail.com>
2020-05-12 14:05:38 +0200

    Added advanced spells and a decorator for them.

---
+++ Engine/Skills/AdvancedSpells/BoltDecorator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Game.Engine.CharacterClasses;
+
+namespace Game.Engine.Skills.BasicSkills
+{
+    [Serializable]
+    class BoltDecorator : SkillDecorator
+    {
+        // decorator for Bolt classes
+        public BoltDecorator(Skill skill) : base(skill.Name, 20, 4, skill)
+        {
+            type = skill.type;
+            MinimumLevel = Math.Max(1, skill.MinimumLevel) + 1;
+            PublicName = $"COMBO - {type} Bolt: a chance equal to your Precision stat to land 0.5*MP damage [{type}] AND " + decoratedSkill.PublicName.Replace("COMBO: ", "");
+            RequiredItem = "Staff";
+        }
+        public override List<StatPackage> BattleMove(Player player)
+        {
+            StatPackage response = new StatPackage(type);
+            Random rnd = new Random();
+            if (rnd.Next(0, 100) < player.Precision)
+            {
+                response.HealthDmg = (int)(0.5 * player.MagicPower);
+                response.CustomText = "You use {type} bolt! (" + (int)(0.5 * player.MagicPower) + $" {type} damage)";
+            }
+            else
+            {
+                response.HealthDmg = 0;
+                response.CustomText = $"You try to use {type} Bolt but it misses!";
+            }
+            List<StatPackage> combo = decoratedSkill.BattleMove(player);
+            combo.Add(response);
+            return combo;
+        }
+    }
+}
---
+++ Engine/Skills/AdvancedSpells/EarthBolt.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Game.Engine.CharacterClasses;
+
+namespace Game.Engine.Skills.AdvancedSpells
+{
+    [Serializable]
+    class EarthBolt : Skill
+    {
+        public static string type = "fire";
+        public EarthBolt() : base("Earth Bolt", 20, 4)
+        {
+            PublicName = $"{Name}: a chance equal to your Precision/2  to land 1*MP damage [{type}]";
+            RequiredItem = "Staff";
+        }
+        public override List<StatPackage> BattleMove(Player player)
+        {
+            StatPackage response = new StatPackage(type);
+            if (Index.RNG(0, 100) < player.Precision / 2)
+            {
+                response.HealthDmg = (int)(0.5 * player.MagicPower);
+                response.CustomText = $"You use {Name}! {(int)(1 * player.MagicPower)} {type} damage)";
+            }
+            else
+            {
+                response.HealthDmg = 0;
+                response.CustomText = $"You try to use {Name} but it misses!";
+            }
+            return new List<StatPackage>() { response };
+        }
+    }
+}
